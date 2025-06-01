a41f09a99960b54e8c790a1dd78cea84176555e8
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-06-01 22:38:04 +0530

    Update spark_1.py

--- spark_1.py
+++ spark_1.py
@@ -8,5 +8,5 @@ if(len(sys.argv) != 3):
 Sc = SparkContext
 file = Sc.textFile(sys.argv[1])
 temp = file.map(lambda line : (int(line[15:19]),int(line[87:92])))
-max = temp.reduceByKey(lambda a,b : a if a<b else b)
+max = temp.reduceByKey(lambda a,b : a if a>b else b)
 max.saveAsTextFile(argv[2])
