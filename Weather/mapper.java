8be6493385da245cda6f1adcead753e539582ee9
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-06-02 11:16:26 +0530

    Create mapper.java

---
+++ Weather/mapper.java
@@ -0,0 +1,15 @@
+package Weather;
+
+import java.util.*;
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, DoubleWritable>{
+  public void map(LongWritable key, Text value, OutputCollector<Text, DoubleWritable> output, Reporter r) throws IOException{
+    String line = value.toString();
+    String year = line.substring(15,19);
+    double temp = Double.parseDouble(line.substring(87,92));
+    output.collect(new Text(year), new DoubleWritable(temp));
+  }
+}
