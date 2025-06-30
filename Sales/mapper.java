8b7e922d8a3a4c9233d0695fbbad23ef48606618
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-06-30 23:18:45 +0530

    Update mapper.java

--- Sales/mapper.java
+++ Sales/mapper.java
@@ -1,4 +1,4 @@
-package Sales;
+package sales;
 
 import java.io.*;
 import org.apache.hadoop.io.*;
@@ -6,13 +6,13 @@ import org.apache.hadoop.mapred.*;
 
 public class mapper extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
   public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter r) throws IOException {
-    String[] line = value.toString().split(",");
-    
-    int price = Integer.parseInt(line[2]);
-    String cardtype = line[3];
-    String country = line[7];
+    String line[] = value.toString().split(",");
 
-    output.collect(new Text(country), new IntWritable(price));   // Total sales per country
-    output.collect(new Text(cardtype), new IntWritable(1));      // Count of card types used
+    String cardtype = line[3];
+    String country = line[8];
+    int price = Integer.parseInt(line[2]);
+
+    output.collect(new Text("Country : "+ country), new IntWritable(price));   // Total sales per country
+    output.collect(new Text("Card Type :"+ cardtype), new IntWritable(1));      // Count of card types used
   }
 }
