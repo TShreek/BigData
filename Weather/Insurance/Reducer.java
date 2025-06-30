b08fbe2634380efc9299329d5bf221990a176978
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-06-30 22:42:59 +0530

    Update Reducer.java

--- Weather/Insurance/Reducer.java
+++ Weather/Insurance/Reducer.java
@@ -7,11 +7,11 @@ import org.apache.hadoop.mapred.*;
 
 public class Reducer extends MapReduceBase
   implements Reducer<Text, IntWritable, Text, IntWritable>{
-    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter r){
+    public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter r) throws IOException {
       int sum=0;
       while(values.hasNext()){
           sum+=values.next().get();
       }
-      output.collect(new Text(key), new IntWritable(sum));
+      output.collect(key, new IntWritable(sum));
     }
   }
