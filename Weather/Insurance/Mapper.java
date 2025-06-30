84053d64fbface6c527d4ad22f9033af79982d39
Shreekrishna T <124446964+TShreek@users.noreply.github.com>
2025-06-30 22:20:22 +0530

    Create Mapper.java

---
+++ Weather/Insurance/Mapper.java
@@ -0,0 +1,16 @@
+package insurance;
+
+import java.io.*;
+import java.util.*;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+
+public class Mapper extends MapReduceBase
+  implements Mapper<LongWritable, Text, Text, IntWritable>{
+    public void map(LongWritable key, Text value, OutputCollector<Text,IntWritable> output, Reporter r){
+      String[] line = value.toString().split(",");
+      string name = line[2];
+      output.collect(new Text(name), new IntWritable(1));
+    }
+  }
